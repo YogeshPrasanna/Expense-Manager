b05c5c73c3d4da6b045107a372d000cc06cb827f
YogeshPrasanna <yogesh15031993@gmail.com>
2017-09-20 15:55:48 +0530

    get all indivigual expenses on save

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 
+  //Add a row 
   $('#addAnother').on('click', function() {
-
     var rowNumber = $('tbody').children().length;
     $('#addExpenseInfo').hide();
-
-    var expenseRow = '<tr id="row' + rowNumber + '"> <th scope="row">' + rowNumber + '</th><td><input type="text" class="form-control datepicker" id="date-row'+ rowNumber +'" placeholder="Date ..." /></td><td><input type="text" class="form-control"</td> <td><div class="dropdown"><button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Category</button><div class="dropdown-menu" aria-labelledby="dropdownMenuButton"><li><a class="dropdown-item" href="#">Automobile</a><a class="dropdown-item" href="#">Clothing</a><a class="dropdown-item" href="#">Entertainment</a><a class="dropdown-item" href="#">Food</a><a class="dropdown-item" href="#">Healthcare</a><a class="dropdown-item" href="#">Vacation</a></li></div></div></td><td><input type="text" class="form-control" id="more-info-row'+ rowNumber +'"  placeholder="description ..." /></td><td><input type="text" class="form-control" id="comments-row'+ rowNumber +'" placeholder="comments ..." /></td></tr>'
+    var expenseRow = '<tr id="row' + rowNumber + '"> <th scope="row">' + rowNumber + '</th><td><input type="text" class="form-control datepicker" id="date-row' + rowNumber + '" placeholder="Date ..." /></td><td><input type="text" class="form-control" /></td> <td><div class="dropdown"><button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Category</button><div class="dropdown-menu" aria-labelledby="dropdownMenuButton"><li><a class="dropdown-item" href="#">Automobile</a><a class="dropdown-item" href="#">Clothing</a><a class="dropdown-item" href="#">Entertainment</a><a class="dropdown-item" href="#">Food</a><a class="dropdown-item" href="#">Healthcare</a><a class="dropdown-item" href="#">Vacation</a></li></div></div></td><td><input type="text" class="form-control" id="more-info-row' + rowNumber + '"  placeholder="description ..." /></td><td><input type="text" class="form-control" id="comments-row' + rowNumber + '" placeholder="comments ..." /></td></tr>'
 
     $('tbody').append(expenseRow);
 
@@ -16,5 +15,40 @@ $(document).ready(function() {
     });
 
     $('.datepicker').datepicker();
+  });
+
+  //save
+  $('#save').on('click', function() {
+
+    //get all the input values
+    var table = $('table');
+    var rowIds = [];
+    var cells = [];
+    var category = [];
+    $('tr', table).each(function() {
+      rowIds.push($(this).attr('id'));
+      $('td input', $(this)).each(function() {
+        cells.push($(this).val());
+      });
+      $('td #dropdownMenuButton', $(this)).each(function() {
+        category.push($(this).text());
+      });
+    });
+
+    //split the input values
+    function split(arr, n) {
+      var res = [];
+      while (arr.length) {
+        res.push(arr.splice(0, n));
+      }
+      return res;
+    }
+
+    var indivigualExpenses = split(cells.slice() ,cells.length/(rowIds.length - 2));
+    indivigualExpenses.forEach(function(elem , i){
+      elem.push(category[i])
+    });
+
+    console.log("row values  ",indivigualExpenses)
   })
 });
