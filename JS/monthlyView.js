34a56f473b64ff418a8bdbbd395b26f07b5794e2
YogeshPrasanna <yogesh15031993@gmail.com>
2017-10-07 19:18:28 +0530

    add category chart in monthy view

--- JS/monthlyView.js
+++ JS/monthlyView.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     var selectedMonth;
+    var currentMonth = new Date().getMonth();
 
     function generateMonthlyRecords(monthNumber) {
         return JSON.parse(localStorage.getItem("expenses")).filter(function(elem) {
@@ -18,6 +19,20 @@ $(document).ready(function() {
         })
     }
 
+    function moneySpentOnACategoryPerMonth(monthNumber, category) {
+        var store =  JSON.parse(localStorage.getItem("expenses")).filter(function(elem) {
+            return (new Date(elem[0]).getMonth()) === monthNumber
+        }).filter(function(el) {
+            return el[4] === category
+        }).map(function(money) {
+            return Number(money[1])
+        })
+
+        return store.length >= 1 ?  store.reduce(function(prev, cur){return prev + cur}) :  store
+    }
+
+    console.log(moneySpentOnACategoryPerMonth(1,"Entertainment "))
+
     // for getting records for that particular month
     // 0 - january to 11 - december
 
@@ -34,52 +49,111 @@ $(document).ready(function() {
     var novemberRecords = generateMonthlyRecords(10);
     var decemberRecords = generateMonthlyRecords(11);
 
+    // var donutChartData = [moneySpentOnACategoryPerMonth(currentMonth,"Automobile "),moneySpentOnACategoryPerMonth(currentMonth,"Clothing "),moneySpentOnACategoryPerMonth(currentMonth,"Entertainment "),moneySpentOnACategoryPerMonth(currentMonth,"Food "),moneySpentOnACategoryPerMonth(currentMonth,"Healthcare "),moneySpentOnACategoryPerMonth(currentMonth,"Vacation ") ];
+    
+    var config = {
+        type: "doughnut",
+        data: {
+            "labels": ["Automobile", "Clothing", "Entertainment", "Food", "Healthcare", "Vacation"],
+            "datasets": [{
+                "label": "My First Dataset",
+                "data": "",
+                "backgroundColor": ["#F17D80", "#737495", "#68A8AD", "#C4D4AF", "#6C8672", "#775BA3"]
+            }]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: ''
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        }
+    }; 
+
+    function paintMonthlyChart(month){
+        var ctx = document.getElementById("this-month-expenses-on-category").getContext("2d");
+        donutChartData = [moneySpentOnACategoryPerMonth(month,"Automobile "),moneySpentOnACategoryPerMonth(month,"Clothing "),moneySpentOnACategoryPerMonth(month,"Entertainment "),moneySpentOnACategoryPerMonth(month,"Food "),moneySpentOnACategoryPerMonth(month,"Healthcare "),moneySpentOnACategoryPerMonth(month,"Vacation ") ]
+        config.data.datasets[0].data = donutChartData
+        window.myDoughnut = new Chart(ctx, config);
+    }
+    
+
     $('#month-jan').on('click', function() {
         printMonthlyRecords('January', januaryRecords)
+        paintMonthlyChart(0)
+
     })
 
     $('#month-feb').on('click', function() {
         printMonthlyRecords('February', februaryRecords)
+        paintMonthlyChart(1)
     })
 
     $('#month-mar').on('click', function() {
         printMonthlyRecords('March', marchRecords)
+        paintMonthlyChart(2)
+
     })
 
     $('#month-apr').on('click', function() {
         printMonthlyRecords('April', aprilRecords)
+        paintMonthlyChart(3)
+
     })
 
     $('#month-may').on('click', function() {
         printMonthlyRecords('May', mayRecords)
+        paintMonthlyChart(4)
+
     })
 
     $('#month-jun').on('click', function() {
         printMonthlyRecords('June', juneRecords)
+        paintMonthlyChart(5)
+
     })
 
     $('#month-jul').on('click', function() {
         printMonthlyRecords('July', julyRecords)
+        paintMonthlyChart(6)
+
     })
 
     $('#month-aug').on('click', function() {
         printMonthlyRecords('August', augustRecords)
+        paintMonthlyChart(7)
+
     })
 
     $('#month-sep').on('click', function() {
         printMonthlyRecords('September', septemberRecords)
+        paintMonthlyChart(8)
+
     })
 
     $('#month-oct').on('click', function() {
         printMonthlyRecords('October', octoberRecords)
+        paintMonthlyChart(9)
+
     })
 
     $('#month-nov').on('click', function() {
         printMonthlyRecords('November', novemberRecords)
+        paintMonthlyChart(10)
+
     })
 
     $('#month-dec').on('click', function() {
         printMonthlyRecords('December', decemberRecords)
+        paintMonthlyChart(11)
+
     })
 
     /* Export to csv */
@@ -150,11 +224,19 @@ $(document).ready(function() {
     /* Export to csv */
 
     /* Export to PDF */
-        $('#exportToPdf').on('click',function(){
-            window.print();
-        })
+    $('#exportToPdf').on('click', function() {
+        window.print();
+    })
     /* Export to PDF */
 
+    
+
+    // window.onload = function() {
+    //     var ctx = document.getElementById("this-month-expenses-on-category").getContext("2d");
+    //     //var allCatMonthly = document.getElementById("all-category-monthly-line-chart").getContext("2d");
+    //     window.myDoughnut = new Chart(ctx, config);
+    //     //window.myLine = new Chart(allCatMonthly, allCatMonthlyconfig);
+    // };
 
 
 })
